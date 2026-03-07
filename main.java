cae9b6e79bded8ea9989aaab162c3b9d4d62067a
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-07 23:44:13 +0500

    fixi

--- main.java
+++ main.java
@@ -28,3 +28,103 @@
 // Name: Ali
 // Monthly Salary: 60000
 // Annual Salary: 720000
+
+
+
+
+
+// Event Registration System
+
+// Features
+// Register Participant
+// View Participants
+// Cancel Registration
+
+// Expected Output
+// ==== Event Registration ====
+
+// 1. Register
+// 2. View Participants
+// 3. Cancel Registration
+
+// Choose option: 1
+
+// Enter Name: Sara
+// Enter Event Name: Tech Conference
+
+// Registration successful!
+
+// View
+
+// --- Participants ---
+
+// Sara - Tech Conference
+// Ali - AI Workshop
+
+
+
+
+
+// Flight Reservation System
+
+// Features
+
+// View Flights
+// Book Flight
+// Cancel Booking
+// View My Ticket
+
+// Expected Output
+// ==== Flight Reservation System ====
+
+// 1. View Flights
+// 2. Book Flight
+// 3. Cancel Booking
+// 4. View Ticket
+
+// Choose option: 1
+
+// --- Available Flights ---
+
+// ID: 1 | Karachi → Lahore | 10:00 AM | Price: 15000
+// ID: 2 | Karachi → Islamabad | 03:00 PM | Price: 18000
+
+// Booking
+
+// Enter Flight ID: 1
+// Enter Passenger Name: Ahmed
+
+// Booking successful!
+// Ticket Price: 15000
+
+
+
+
+
+
+5️⃣ Inventory & Stock Management System 📦
+Features
+
+Add Product
+
+Update Stock
+
+Sell Product
+
+View Inventory
+
+Expected Output
+===== Inventory System =====
+
+1. Add Product
+2. Update Stock
+3. Sell Product
+4. View Inventory
+
+Sell Product
+
+Enter Product Name: Laptop
+Enter Quantity: 2
+
+Sale successful!
+Remaining Stock: 8

247f76381cde688b6595f10c202b373693eff4d0
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-07 23:38:26 +0500

    init

---
+++ main.java
@@ -0,0 +1,30 @@
+// Employee Payroll System
+
+// Features
+// Add Employee
+// View Employees
+// Calculate Salary
+// Remove Employee
+// Expected Output
+
+// ==== Employee Payroll System ====
+
+// 1. Add Employee
+// 2. View Employees
+// 3. Calculate Salary
+
+// Choose option: 1
+
+// Enter Employee ID: 501
+// Enter Name: Ali
+// Enter Monthly Salary: 60000
+
+// Employee added successfully
+
+// Salary Calculation
+
+// Enter Employee ID: 501
+
+// Name: Ali
+// Monthly Salary: 60000
+// Annual Salary: 720000
